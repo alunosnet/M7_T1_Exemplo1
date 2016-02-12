ed96abe6dd3ad2a34655c4b23383cc8a3d974b9e
alunosnet <alunosnet@gmail.com>
2016-02-12 08:47:38 +0000

    Final do primeiro dia

---
+++ m7_t1_exemplo1/MasterPage.Master.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace m7_t1_exemplo1
+{
+    public partial class MasterPage : System.Web.UI.MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //verificar se existe o cookie
+            HttpCookie cookie = Request.Cookies["aviso"] as HttpCookie;
+            if (cookie != null)
+            {
+                //esconder o aviso
+                div_aviso.Visible = false;
+            }
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            //criar um cookie
+            HttpCookie cookie = new HttpCookie("aviso","mostrado");
+            //definir o prazo de validade do cookie
+            cookie.Expires = DateTime.Now.AddYears(1);
+            //enviar cookie para o cliente
+            Response.Cookies.Add(cookie);
+            //esconder o aviso
+            div_aviso.Visible = false;
+        }
+    }
+}
---
+++ m7_t1_exemplo1/index.aspx
@@ -0,0 +1,7 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage.Master" AutoEventWireup="true" CodeBehind="index.aspx.cs" Inherits="m7_t1_exemplo1.index" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+<title>Página inicial</title>
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+<h1>Página inicial</h1>
+</asp:Content>
